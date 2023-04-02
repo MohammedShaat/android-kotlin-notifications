81776d541a2fb07920d290cca5e71d13933beb0b
Mohammed Shaat <mohammedshaat.it@gmail.com>
2023-04-02 21:23:20 +0200

    Send FCM Notification and Data

--- app/src/main/java/com/example/android/eggtimernotifications/util/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/MyFirebaseMessagingService.kt
@@ -1,10 +1,23 @@
 package com.example.android.eggtimernotifications.util
 
+import android.app.Notification
+import android.app.NotificationManager
 import android.util.Log
+import androidx.core.content.ContextCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
-    override fun onNewToken(p0: String?) {
-        Log.i("NewToken", "$p0")
+    override fun onNewToken(token: String?) {
+        Log.i("NewToken", "$token")
     }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        remoteMessage?.data?.let {
+            Log.i("onMessageReceived", "$it")
+        }
+
+    }
+
+
 }
